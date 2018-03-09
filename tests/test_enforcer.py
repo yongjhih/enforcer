8ba53efa1f7c66c312a88269e2398dc2825a0e76
Andrew Chen <yongjhih@gmail.com>
2018-03-09 00:39:32 -0800

    Add test cases

--- tests/test_enforcer.py
+++ tests/test_enforcer.py
@@ -9,6 +9,9 @@ def safe_doubler(x):
     y: int = x * 2
     return y
 
-def test_safe():
+def test_safe_error():
     with pytest.raises(TypeViolationError):
         safe_doubler('1')
+
+def test_safe():
+    assert safe_doubler(1) == 2
